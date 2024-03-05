c9711b265a9e252744476de8dab0527d2008f6f4
Bacalhau Seco <64091315+bacalhau-seco@users.noreply.github.com>
2024-03-05 11:54:19 +0000

    added the script

---
+++ stream.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+VBR="1500k"
+FPS="24"
+QUAL="superfast"
+
+YOUTUBE_URL="the youtube URL"
+KEY="your live youtube key"
+
+VIDEO_SOURCE="video file location"
+AUDIO_SOURCE="audio file location"
+
+ffmpeg \
+    -re -f lavfi -i "movie=filename=$VIDEO_SOURCE:loop=0, setpts=N/(FRAME_RATE*TB)" \
+    -thread_queue_size 512 -stream_loop 1 -i "$AUDIO_SOURCE" \
+    -map 0:v:0 -map 1:a:0 \
+    -map_metadata:g 1:g \
+    -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
+    -acodec libmp3lame -ar 44100 -threads 6 -qscale:v 3 -b:a 320000 -bufsize 512k \
+    -f flv "$YOUTUBE_URL/$KEY"
